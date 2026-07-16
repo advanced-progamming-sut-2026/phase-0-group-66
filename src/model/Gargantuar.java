de2621e086b8b08e4cab585ca0abfaedb4e4d04e
Mohammad Parsa Hosseini <mphosseini84@gmail.com>
2026-07-16 12:28:20 +0330

    Update Gargantuar.java

--- src/model/Gargantuar.java
+++ src/model/Gargantuar.java
@@ -1,11 +1,26 @@
 package model;
 
+import java.util.List;
+
 public class Gargantuar extends Zombie {
     private boolean impReady;
 
+    public Gargantuar(ZombieDefinition definition, List<Armor> armors) {
+        super(definition, armors);
+        impReady = true;
+    }
+
     public void smashPlant(Plant plant) {
+        if (plant != null) {
+            plant.takeDamage(Math.max(plant.getHealth(), damage));
+        }
     }
 
     public void throwImp() {
+        impReady = false;
+    }
+
+    public boolean isImpReady() {
+        return impReady;
     }
 }
