4bf05e141e5cf062ee936aeaceae29a9df3ba044
Mohammad Parsa Hosseini <mphosseini84@gmail.com>
2026-08-15 22:10:51 +0330

    Ready for Phase 2

---
+++ lwjgl3/src/main/java/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,30 @@
+package lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import pvz.PvzApplication;
+
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) {
+            return;
+        }
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new PvzApplication(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("phase-0-group-66");
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(
+            Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1
+        );
+        configuration.setWindowedMode(1280, 720);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
