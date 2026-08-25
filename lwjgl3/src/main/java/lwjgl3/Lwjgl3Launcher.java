53226845818f682244efb6e52d82bd6baba58507
Mohammad Parsa Hosseini <mphosseini84@gmail.com>
2026-08-25 23:22:30 +0330

    Update Lwjgl3Launcher.java

--- lwjgl3/src/main/java/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/lwjgl3/Lwjgl3Launcher.java
@@ -24,7 +24,6 @@ public class Lwjgl3Launcher {
             Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1
         );
         configuration.setWindowedMode(1280, 720);
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
 }
